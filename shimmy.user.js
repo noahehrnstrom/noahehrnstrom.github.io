991b75dd04cfb569c611849da8b0057244e58a66
noahehrnstrom <noah.ehrnstrom@gmail.com>
2023-07-06 19:45:02 +0200

    Add chatguessr something

---
+++ shimmy.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Shimmy
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  The Shimmy bot
+// @author       Dionysios of Sirakusa
+// @match        https://chatguessr.com/map/plonkit
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=chatguessr.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.location.replace("https://chatguessr.com/map/Shimmy%F0%9F%A4%96");
+})();
